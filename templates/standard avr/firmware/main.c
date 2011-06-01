f4be420de2612a4281deccbfc87919f5a7af27f5
m9dfukc <andreas@invertednothing.com>
2011-06-01 18:53:13 +0200

    initial import

---
+++ templates/standard avr/firmware/main.c
@@ -0,0 +1,24 @@
+/*
+ *  main.c
+ *  «PROJECTNAME»
+ *
+ *  Created by «FULLUSERNAME» on «DATE».
+ *  Copyright «YEAR» «COMPANY». All rights reserved.
+ *
+ */
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+
+// Defines
+#define FOREVER 1
+
+int main(void)
+{
+    while (FOREVER)
+    {
+        // Your code here...
+    }
+    return 0;
+}
